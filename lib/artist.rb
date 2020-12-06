a54dcb9a2f51d8da443979963e9906382f54060c
wei chen <weichen9083@gmail.com>
2020-12-06 20:43:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,32 @@
 class Artist
   
-  attr_accessor :name 
   
-  def initialize(name)
-    self.name = name 
-  end 
-  
+      attr_accessor :name
+      
+          @@song_count = 0
+     
+      def initialize(name)
+        @name = name
+      end
+     
+      def add_song(song)
+        song.artist = self
+         @@song_count +=1
+      end
+     
       def songs
         Song.all.select {|song| song.artist == self}
       end
-  
+      
   def add_song_by_name(name)
-    x = Song.new(name)
-    
-  end 
-
+    song = Song.new(name)
+    add_song(song)
+      @@song_count +=1
+  end
+  
+def self.song_count
+    @@song_count
+  end
 
       
 end
